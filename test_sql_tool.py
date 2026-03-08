eff428e099dd121454ddbacf573064639b7c0627
António Cruz <appaiscruz@gmail.com>
2026-03-08 22:59:10 +0000

    localhost verification

--- test_sql_tool.py
+++ test_sql_tool.py
@@ -1,3 +1,12 @@
+import os
+
+from dotenv import load_dotenv
+
+# Load .env and use localhost when running this script on the host (outside Docker)
+load_dotenv()
+if os.getenv("SQL_HOST") == "db_sql":
+    os.environ["SQL_HOST"] = "localhost"
+
 from src.api.tools.sql_query_tool import sql_query_tool
 
 query = "SELECT * FROM diseases LIMIT 5;"
